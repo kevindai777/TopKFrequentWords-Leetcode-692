53184594662e8c086e88922789dd36139340794e
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 07:33:49 -0400

    edits to explanation

--- topkfrequentwords.js
+++ topkfrequentwords.js
@@ -1,6 +1,7 @@
 //Topic of the question is to find the top "k" frequent elements (where k is a positive integer) in an array.
 //If two elements have the same frequency, return them in alphabetic order.
 
+
 //O(n) solution
 let words = ["i", "love", "leetcode", "i", "love", "coding"]
 let k = 2
@@ -16,9 +17,12 @@ for (let i = 0; i < words.length; i++) {
     }
 }
 
+//Sort the word frequencies
 let sorted = Object.keys(map).sort((a,b) => {
+    //If they have the same frequency, return them alphabetically
     if (map[a] === map[b]) {
         return a > b ? 1 : -1
+    //If not, return them in descending order
     } else {
         return map[b] - map[a]
     }
